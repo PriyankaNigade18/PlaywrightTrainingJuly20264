efc102ba9aeb4521a846a64e1e7e5947c40b8836
PriyankaNigade18 <piyu1818@gmail.com>
2026-07-12 18:13:09 +0530

    Javascript Day-1

---
+++ JsBasics/P001_FirstCode.js
@@ -0,0 +1,18 @@
+
+//single line comment: this is not excuting
+
+/*
+Multiple lines comment
+----------------------------
+How to print any statement is JS
+
+Syntax:
+---------
+console.log("statement");
+
+*/
+
+
+console.log("Hello All!");
+console.log("Welcome to learn Javascript in Detail!");
+
